1096c202e43e6bc6bc1ecf8c1acdc42c165d991b
Shaahin Chaichi <shaahin.chaichi@gmail.com>
2023-07-15 18:26:58 +0330

    Start

--- main.py
+++ main.py
@@ -4,4 +4,13 @@ import requests
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
 response = requests.get(URL)
-movie_web_page = response.text
+website_html = response.text
+
+soup = BeautifulSoup(website_html, "html.parser")
+all_movies = soup.findAll(name="h3", class_="title")
+movie_titles = [movie.getText() for movie in all_movies][::-1]
+
+with open(file="movies.txt", mode="w", encoding="utf-8") as file:
+    for movie in movie_titles:
+        file.write(f"{movie}\n")
+
