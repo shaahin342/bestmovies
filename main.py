e88cab5e4d348da6725d03f3022503fc0f2c68e7
Shaahin Chaichi <shaahin.chaichi@gmail.com>
2023-07-15 17:28:05 +0330

    Start

---
+++ main.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup
+import requests
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+response = requests.get(URL)
+movie_web_page = response.text
